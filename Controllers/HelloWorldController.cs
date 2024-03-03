dddd1c8b6658b5c66e6698a040db9e5371327278
DjoKap <129984119+DjoKap@users.noreply.github.com>
2024-03-03 14:43:49 -0600

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCFirstApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string index()
+        {
+            return "This is my default action...";
+        }
+        public string Welcome(string name, int num)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, Number var is:{num}");
+            // controller-name/action=index/id?
+        }
+    }
+
+
+}
